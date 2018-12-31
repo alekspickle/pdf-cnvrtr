6c849785de0098a18e580259fe3674b50fc9e34d
alexrow <lesha.gnat@gmail.com>
2018-12-31 03:43:34 +0200

    finish logic

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-const puppeteer = require("puppeteer");
+const fs = require("fs");
+const utf8 = require("utf8");
 const hummus = require("hummus");
-const standard_input = process.stdin;
-standard_input.setEncoding("utf-8");
+const stdin = process.stdin;
+const puppeteer = require("puppeteer");
+const path = require("path");
+
+stdin.setEncoding("utf-8");
 
 async function generatePdf(url, outputDir, filename) {
   const browser = await puppeteer.launch();
@@ -11,37 +15,47 @@ async function generatePdf(url, outputDir, filename) {
   //   if (request.url.includes("name-you-can-filter")) request.abort();
   //   else request.continue();
   // });
-
-  await page.goto(url, {
-    waitUntil: "networkidle",
-    networkIdleTimeout: 5000,
-    timeout: 30000
-  });
-  await page.pdf({
-    path: path.join(outputDir, filename),
-    format: "A4"
-  });
-  browser.close()
+  await page.goto(url, { waitUntil: ["domcontentloaded"] });
+  try {
+    await page.pdf({
+      path: path.join(outputDir, filename),
+      format: "A4"
+    });
+  } catch (e) {
+    console.log("err", e);
+  }
+  browser.close();
 }
 
-function combinePdfs(files){
-  const pdfWriter = hummus.createWriter('newsletter.pdf');
+function combinePdfs(files) {
+  const pdfWriter = hummus.createWriter("./output/combined.pdf");
   files
-      .filter(file => file.endsWith(".pdf"))
-      .forEach(fn => {
-          pdfWriter.appendPDFPagesFromPDF(file);
-      });
-
+    .filter(file => file.endsWith(".pdf"))
+    .forEach(fn => pdfWriter.appendPDFPagesFromPDF(fn));
   pdfWriter.end();
 }
+
 console.log("Please enter the url, you wish to get in PDF: ");
-standard_input.on("data", data => {
-  if (data === "exit\n") {
+stdin.on("data", data => {
+  //get actual data, instead of data+0D+0A
+  const actualData = decodeURI(encodeURI(data).replace(/\%0D\%0A/, ""));
+
+  if (actualData === "combine") {
+    fs.readdir("./output", (err, items) => {
+      console.log(items);
+      if (items.length) combinePdfs(items);
+    });
+  } else if (actualData === "exit" || actualData === "q") {
     console.log("Process completed.");
     process.exit();
-  } else if(data.includes('http://') || data.includes('https://')){
-    generatePdf(data, './output','newpdf.pdf')
-  } else {
-    console.log("User Input Data : " + data);
+  } else if (
+    actualData.includes("http://") ||
+    actualData.includes("https://")
+  ) {
+    const date = Date.now()
+      .toString()
+      .slice(9);
+    generatePdf(actualData, "./output", `newpdf${date}.pdf`);
+    console.log("enter another url, or type q to exit. ");
   }
 });
